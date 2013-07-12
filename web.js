10f2375db47d83a1b2d0f1446a4fa32030464f9c
rnalakurthi <rknalakurthi@gmail.com>
2013-07-12 17:24:16 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
+app.get('/', fs.readFileSync('index.html'););
+<!--app.get('/', function(request, response) {
   response.send('Hello World2!');
-});
+});-->
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
